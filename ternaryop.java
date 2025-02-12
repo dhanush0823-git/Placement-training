3a840c923f0d9afc72ab58313f31faaaafb77da3
dhanush0823-git <dhanushhemapriya0823@gmail.com>
2025-02-12 12:06:22 +0530

    Add files via upload

---
+++ ternaryop.java
@@ -0,0 +1,12 @@
+
+class Main
+{
+    public static void main (String s[])
+    {
+        int a=10;
+        int b=23;
+        int c=58;
+       System.out.println("maximum="+((a>b&&a>c)?a:(b>c)?b:c));
+        
+    }
+}
