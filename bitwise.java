b370ba5612b46abe92d02ac9c6272c2dd8baca9b
dhanush0823-git <dhanushhemapriya0823@gmail.com>
2025-02-12 12:42:27 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,14 @@
+
+class Main
+{
+    public static void main (String s[])
+    {
+        int a=492;
+        int b=333;
+       
+       System.out.println("AND OPERATOR="+(a&b));
+       System.out.println("OR OPERATOR="+(a|b));
+       System.out.println("EXOR OPERATOR="+(a^b));
+        
+    }
+}
