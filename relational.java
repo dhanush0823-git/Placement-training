81ab73a7c0bb25b170144e16ec44ce922ef5433b
dhanush0823-git <dhanushhemapriya0823@gmail.com>
2025-02-12 11:55:12 +0530

    Add files via upload

---
+++ relational.java
@@ -0,0 +1,16 @@
+
+class Main
+{
+    public static void main (String s[])
+    {
+        int a=10;
+        int b=20;
+        System.out.println("a<b="+(a<b));
+         System.out.println("a>b="+(a>b));
+          System.out.println("a==b="+(a==b));
+           System.out.println("a<=b="+(a<=b));
+            System.out.println("a>=b="+(a>=b));
+             System.out.println("a!=b="+(a!=b));
+        
+    }
+}
