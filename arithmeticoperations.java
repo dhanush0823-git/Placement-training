30ebf3e9c870055cf4668b2843836cc513290d0b
dhanush0823-git <dhanushhemapriya0823@gmail.com>
2025-02-12 11:47:52 +0530

    Add files via upload

---
+++ arithmeticoperations.java
@@ -0,0 +1,13 @@
+ class main 
+ {
+     public static void main(String s[])
+     {
+        int a=100;
+        int b=98;
+        System.out.println("a+b="+(byte)(a+b));
+        System.out.println("a-b="+(byte)(a-b));
+        System.out.println("a*b="+(byte)(a*b));
+        
+     }
+ }
+ 
---
+++ maximum3numbers.java
@@ -0,0 +1,11 @@
+
+class Main
+{
+    public static void main(String s[])
+    {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println("a<b&&a>c="+(a<b&&a>c));
+    }
+}
