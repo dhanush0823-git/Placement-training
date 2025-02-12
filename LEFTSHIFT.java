18e683724313d505861454f50e5094b0eeb54f87
dhanush0823-git <dhanushhemapriya0823@gmail.com>
2025-02-12 13:01:00 +0530

    Add files via upload

---
+++ LEFTSHIFT.java
@@ -0,0 +1,13 @@
+
+class Main
+{
+    public static void main (String s[])
+    {
+        int a=10;
+       
+       
+       System.out.println("LEFT SHIFT="+(a<<2));
+      
+        
+    }
+}
---
+++ RIGHTSHIFT.java
@@ -0,0 +1,13 @@
+
+class Main
+{
+    public static void main (String s[])
+    {
+        int a=10;
+       
+       
+       System.out.println("RIGHT SHIFT="+(a>>2));
+      
+        
+    }
+}
