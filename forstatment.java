4b38191a768c06074edb1070a8b47eb5ab3cc89b
dhanush0823-git <dhanushhemapriya0823@gmail.com>
2025-02-12 16:06:59 +0530

    Add files via upload

---
+++ forstatment.java
@@ -0,0 +1,16 @@
+class Main
+{
+    public static void main(String []args)
+    {
+        int a=7;
+        for(;;)
+        {
+        if(a>3)
+            System.out.println(a);
+        else
+            break;
+            a--;
+        }
+    }
+}
+        
