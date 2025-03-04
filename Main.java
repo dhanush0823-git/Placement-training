3fd9e43dd7b2bf0b203b054782c587e37f373ff6
dhanush0823-git <dhanushhemapriya0823@gmail.com>
2025-03-04 10:50:09 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,31 @@
-import java.util.Scanner;
-class Add
-{ 
-    private int a,b,c;
-    public void Sum()
-    {
-        Scanner in=new Scanner(System.in);
-        a=in.nextInt();
-        b=in.nextInt();
-        c=a+b;
-        System.out.println("Sum="+c);
-    }
-}
-class Main
+class Main 
 {
-    public static void main(String s[])
+    public static void main (String []args)
     {
-        Add obj=new Add();
-        obj.Sum();
+        String str = "Hey ";
+        str = str.concat ("Dhanush!");
+        System.out.println("String : " + str);
+        
+        
+        StringBuilder sb = new StringBuilder(" Hello ");
+        sb.append("Everyone");
+        System.out.println("StringBuilder :"+sb);
+        
+        StringBuffer buff = new StringBuffer (" Hello ");
+        buff.append("Triloch");
+        System.out.println("StringBuffer :"+buff);
+        
+        char[] chars = {'D','h','a','n','u','s','h','!'};
+
+        String result =" ";
+        for( char c : chars)
+        {
+                    result += c;
+
+
+        }
+        System.out.println("Without Builtin :"+result);
+        
+        
     }
 }
